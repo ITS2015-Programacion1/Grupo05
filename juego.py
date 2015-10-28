ae97102fd92ccad99df87ad936b611c373b08480
lucassalva <mijuki2010@live.com.ar>
2015-10-28 14:07:19 -0300

    agregarle efecto al personaje

--- juego.py
+++ juego.py
@@ -8,21 +8,35 @@ fin_de_juego = False
 #Iniciar el motor de Pilas Engine
 pilas = pilasengine.iniciar()
 #Dotar de un Fondo 
-Fondo = pilas.fondos.Noche()
+fondo = pilas.fondos.Galaxia()
 # Añadir un marcador
 puntos = pilas.actores.Puntaje(x=230, y=200, color=pilas.colores.rojo)
 puntos.magnitud = 40
 # Añadir el conmutador de Sonido
 pilas.actores.Sonido()
 
+# Crear personaje
+class android(pilasengine.actores.Actor):
 
-class personaje(pilasengine.actores.Actor):
-
-    def iniciar(self):
-        self.imagen = "actor.ico"
+	def iniciar(self):
+		self.imagen = "actor.ico"
 		self.escala = 0.7
 		self.x = -210
 		self.y = 0
-pilas.actores.vincular(personaje)
-personaje = pilas.actores.personaje()
+pilas.actores.vincular(android)
+android = pilas.actores.android()
+
+
+#agregar efecto al personaje
+android.escala = 1
+android.rotacion = 0
+
+def girar():
+	android.rotacion = android.rotacion + 1
+	return True
+
+
+
+
+pilas.escena.tareas.siempre(0.01, girar)
 
