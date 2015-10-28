0e96fee8e671c895ff4af95ac5c9414556ce4f13
lucassalva <mijuki2010@live.com.ar>
2015-10-28 11:28:36 -0300

    se crea el personaje y se lo posiciona

--- juego.py
+++ juego.py
@@ -1,1 +1,17 @@
+import pilasengine
+pilas.fondos.Noche()
+pilas = pilasengine.iniciar()
+
+class personaje(pilasengine.actores.Actor):
+
+    def iniciar(self):
+        self.imagen = "actor.ico"
+	self.escala = 0.7
+	self.x = -210
+	self.y = 0
+pilas.actores.vincular(personaje)
+personaje = pilas.actores.personaje()
+
+
+
 
