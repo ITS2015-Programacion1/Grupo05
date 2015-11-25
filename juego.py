dcc4696705ce202d8bad640b9181f74248c99c5c
emiborionicab <emiborionicab@gmail.com>
2015-11-25 13:57:48 -0300

    Se corrigio y perfecciono el funcionamiento del Menu.py

--- juego.py
+++ juego.py
@@ -1,27 +1,42 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import pilasengine
-#menu
+import random
+
+Fin_del_juego = False
+#Iniciar el motor de PilasEngine
+pilas = pilasengine.iniciar(ancho=288, alto=511)
+#Añadir un marcador
+puntos = pilas.actores.Puntaje(x=100, y=240, color=pilas.colores.naranja)
+puntos.magnitud = 40
+#Añadir el conmutador de sonido
+Sonido = pilas.actores.Sonido
+#Variables y constantes
+Tubos1 = []
+Tubos2 = []
+Pisos = []
+Puntos = []
+#Define todas las cualidades del Fondo
 class FondoConMovimiento(pilasengine.fondos.Fondo):
 
     def iniciar(self):
-    	 # define que imagen se usara como fondo
+         # define que imagen se usara como fondo
         self.imagen = 'flappyfondo.png' 
         # velocidad a la que se movera el fondo
         self.velocidad = 1  
 
     def actualizar(self):
-    	# define que se movera hacia la izquierda
+        # define que se movera hacia la izquierda
         self.x -= self.velocidad  
-
         # Si el fondo llega al borde izquierdo de la pantalla entonces cambiar su posicion en X
         if self.x + self.ancho <= 0:
             self.x = self.ancho
 
+#Define todas las cualidades de los Tubos (Colisionadores)
 class Tubo(pilasengine.actores.Actor):
 
     def iniciar(self):
-    	# Define la imagen que se usara como Tubo
+        # Define la imagen que se usara como Tubo
         self.imagen = 'flappytubo.png'
         # Define velocidad con la que se movera el tubo  
         self.velocidad = 1  
@@ -29,78 +44,103 @@ class Tubo(pilasengine.actores.Actor):
         self.x = 288 
 
         # posicion en Y aleatoria entre 159 y 250
-        self.y = self.pilas.azar(159, 250)
-
+        self.y = self.pilas.azar(95, 350)
     def actualizar(self):
-    	# mover tubos constantemente
+        # mover tubos constantemente
         self.x -= self.velocidad  
-
         # Si el tubo llega al al borde izquierdo de la pantalla entonces lo eliminamos
         if self.x + self.ancho <= -144:
-             self.eliminar()
-def crear_tubos():
-	#funcion que sera llamada como una tarea
-    tubo1 = Tubo(pilas)
-    #Tubo de abajo
-    tubo2 = Tubo(pilas)
-    tubo2.rotacion = 180
-    tubo2.y = tubo1.y - (tubo1.alto / 2) * 2 - 100
+            self.eliminar()
+    def crear_tubos():
+        #funcion que sera llamada como una tarea
+        tubo1 = Tubo(pilas)
+        #Define que el radio de colision sea rectangular
+        colision_rec1 = pilas.fisica.Rectangulo(-1000,-1000,50,315, sensor=True, dinamica= False)
+        tubo1.figura_de_colision = colision_rec1
+        #Agrega los tubos al vector, para asi de este modo se puedan eliminar luego
+        Tubos1.append(tubo1)
+        #Tubo de abajo
+        tubo2 = Tubo(pilas)
+        #Define que el radio de colision sea rectangular
+        colision_rec2 = pilas.fisica.Rectangulo(-1000,-1000,50,315, sensor=True, dinamica= False)
+        tubo2.figura_de_colision = colision_rec2
+        #Define que el tubo debe rotar para poder quedar con la forma deseada de tubo
+        tubo2.rotacion = 180
+        #Define la distancia 
+        tubo2.y = tubo1.y - (tubo1.alto / 2) * 2 - 100
+        #Agrega los tubos al vector, para asi de este modo se puedan eliminar luego
+        Tubos2.append(tubo2)
+        #Llamar a la funcion crear_tubos cada 2.5 segundos
+    pilas.tareas.siempre(4, crear_tubos)
+#Define todas las cualidades del Piso (Colisionadores)
+class Piso(pilasengine.actores.Actor):
+    def iniciar(self):
+        #Define la imagen que se usara como Piso
+        self.imagen = 'flappypiso.png'
+        #Define la velocidad con la que se movera el Piso
+        self.velocidad = 1
+        #Posicion X en la que aparecera
+        self.x = 288
+        #Posicion Y en la que aparecera
+        self.y = -270
+    def actualizar(self):
+        # mover tubos constantemente
+        self.x -= self.velocidad  
+        # Si el Piso llega al al borde izquierdo de la pantalla entonces lo eliminamos
+        if self.x + self.ancho <= -144:
+            self.eliminar()
+    def crear_piso():
+        #Funcion que será llamada como Tarea
+        piso1 = Piso(pilas)
+        #Define que la colision sea rectangular
+        colision_rec9 = pilas.fisica.Rectangulo(-1000,-1000,185,100, sensor=True, dinamica= False)
+        piso1.figura_de_colision = colision_rec9
+        #Agrega el piso a un vector, para que de este modo se pueda eliminar luego
+        Pisos.append(piso1)
+        #Llamar a la funcion crear_piso cada 2.5 segundos
+    pilas.tareas.siempre(3, crear_piso)  
+
+class ganar_puntos(pilasengine.actores.Actor):
+    def iniciar(self):
+        # Define la imagen que se usara como Actor para generar puntos
+        self.imagen = 'invisible.png'
+        # Define velocidad con la que se movera el tubo  
+        self.velocidad = 1  
+        # Posicion en X en la que aparecera
+        self.x = 288 
+        # posicion en Y aleatoria entre -160 y -30
+        self.y = self.pilas.azar(-160, -30)
+    def actualizar(self):
+        # mover puntos constantemente
+        self.x -= self.velocidad  
+        # Si el tubo llega al al borde izquierdo de la pantalla entonces lo eliminamos
+        if self.x + self.ancho <= -144:
+            self.eliminar()
+def crear_ganar_puntos():
+    gana = ganar_puntos(pilas)
+    #Define que la colision sea rectangular
+    colision_rec7 = pilas.fisica.Rectangulo(-1000,-1000,50,95, sensor=True, dinamica= False)
+    gana.figura_de_colision = colision_rec7
+    #Agrega crear_ganar_puntos a un vector para ser eliminados luego
+    Puntos.append(gana)
+    #Aumenta el contador en 1
+    puntos.aumentar(1)
+    #Llama a la funcion crear_ganar_piso en 2.5 segundos 
+pilas.tareas.siempre(4, crear_ganar_puntos)
 
 
-pilas = pilasengine.iniciar(ancho=288, alto=511)
 # Crear fondo 1 con posicion en X de 288 (no visible)
 fondo55 = FondoConMovimiento(pilas)
 fondo55.x = 288
 # Crear fondo 2
 fondo22 = FondoConMovimiento(pilas)
-# Crear fondo 1 con posicion en X de 288 (no visible)
-fondo = FondoConMovimiento(pilas)
-fondo.x = 288
-
-# Crear fondo 2
-fondo2 = FondoConMovimiento(pilas)
-fondo3 = FondoConMovimiento(pilas)
-# llamar a la funcion crear_tubos cada 2.5 segundos
-pilas.tareas.siempre(4, crear_tubos)
-
-class SaltarUnaVez(pilas.comportamientos.Comportamiento):
-    """Realiza un salto, cambiando los atributos 'y'."""
-
-    def iniciar(self, receptor, velocidad_inicial=10, cuando_termina=None):
-        """Se invoca cuando se anexa el comportamiento a un actor.
-
-        :param receptor: El actor que comenzará a ejecutar este comportamiento.
-        """
-        super(SaltarUnaVez, self).iniciar(receptor)
-        self.velocidad_inicial = velocidad_inicial
-        self.cuando_termina = cuando_termina
-        self.sonido_saltar = self.pilas.sonidos.cargar("audio/saltar.wav")
-        self.suelo = int(self.receptor.y)
-        self.velocidad = self.velocidad_inicial
-        self.sonido_saltar.reproducir()
-        self.velocidad_aux = self.velocidad_inicial
-        self.receptor.saltando = True
-
-    def actualizar(self):
-        self.receptor.y += self.velocidad
-        self.velocidad -= 0.3
-
-        if self.receptor.y <= self.suelo:
-            self.velocidad_aux /= 3.5
-            self.velocidad = self.velocidad_aux
-
-            if self.velocidad_aux <= 1:
-                # Si toca el suelo
-                self.receptor.y = self.suelo
-                if self.cuando_termina:
-                    self.cuando_termina()
-                self.receptor.saltando = False
-                return True
+#Define los controles que usara el juego
 teclas = {
             pilas.simbolos.w: 'arriba',
-            pilas.simbolos.ESPACIO: 'boton',
+            pilas.simbolos.s: 'abajo',
         }
 mi_control = pilas.control.Control(teclas)
+#Define al personaje principal FlappyBird
 class FlappyConControles(pilasengine.actores.Actor):
     #Clase que define todos los aspectos del Actor
     def iniciar(self):
@@ -112,40 +152,61 @@ class FlappyConControles(pilasengine.actores.Actor):
         self.x = -80
         #Define la posicion Y en la cual se posicionara el actor
         self.y = -200
-        #Define la posicion X en la cual se posicionara la camara
-        pilas.camara.x = [0]
-        self.saltando = False
-
-<<<<<<< HEAD
-class FlappyConControles(pilasengine.actores.Actor):
-	#Clase que define todos los aspectos del Actor
-    def iniciar(self):
-    	#Define la imagen que se utilizara para el actor"
-        self.imagen = pilas.imagenes.cargar_grilla("flappybird.png", 3)
-        #Define la escala de tamaño del actor
-        self.escala = 1.2
-        #Define la posicion X en la cual se posicionara el actor
-        self.x = -80
-        #Define la posicion Y en la cual se posicionara el actor
-        self.y = -200
-        #Define la posicion X en la cual se posicionara la camara
-        pilas.camara.x = [0]
-        self.saltando = False
-
-=======
->>>>>>> d5e322e9dd610c117fe66a2aca1d79092580521d
+        
     def actualizar(self):
+        #Define cuanto avanzara en 0
         self.x += 0
         #Define cuanto avanzara el actor
         self.imagen.avanzar()
-        #Define cuanto avanzara la camara
-        pilas.camara.x += .001
         #Define los controles
+        #Define que pasara cuando se toque la letra "S"
+        if mi_control.abajo:
+            #Define cuanto se movera en el eje Y, mediante la interpolacion
+            self.v=self.y-20
+            #Define cuanto se movera el personaje gracias a esta interpolacion
+            pilas.utils.interpolar(FlappyBird,'y',self.v,0.1)
+        #Define que pasara cuando se toque la letra "W"
         if mi_control.arriba:
-            self.y +=1
-            if not self.saltando:
-                self.hacer("SaltarUnaVez")
-FlappyConControles(pilas)
-pilas.comportamientos.vincular(SaltarUnaVez)
+            #Define cuanto se movera en el eje Y, mediante la interpolacion
+            self.z=self.y+90
+            #Define cuanto se movera el personaje gracias a esta interpolacion
+            pilas.utils.interpolar(FlappyBird,'y',self.z,0.4)
+            #Define que sonido se escuchara cuando se toque la tecla "W"
+            self.sonido_saltar = self.pilas.sonidos.cargar("audio/saltar.wav")
+            #Le indica que se comiense a reproducir el sonido del salto
+            self.sonido_saltar.reproducir()
+        
+#Funcion utilizada para cuando el usuario Pierde
+def Perder(FlappyBird, tubos):  
+    global Fin_del_juego
+    FlappyBird.y = 0
+    tubos.eliminar()
+    #Emerger mensaje cuando el FlappyBird colisiona con un tubo
+    texto1=pilas.actores.Texto("GAME OVER")
+    texto1.y= 100
+    #Informarle al usuario sobre su puntuacion obtenida
+    texto2=pilas.actores.Texto("Perdiste conseguiste %d puntos" % (puntos.obtener()))
+    texto2.y= -100
+    #Define que el juego a finalizado.
+    Fin_del_juego=True
+
+    if Fin_del_juego:
+        return False
+    else:
+        return True
+#Vincula a FlappyConControles como un Actor
+pilas.actores.vincular(FlappyConControles)
+#Vincula a Tubo como un Actor
+pilas.actores.vincular(Tubo)
+#Vincula a Piso como un Actor
+pilas.actores.vincular(Piso)
+#Define como se llamara FlappyConControles
+FlappyBird = FlappyConControles(pilas)
+#Colision del actor FlappyBird con los Tubos1 y de esta manera ejecuta Perder
+pilas.colisiones.agregar(FlappyBird, Tubos1, Perder)
+#Colision del actor FlappyBird con los Tubos2 y de esta manera ejecuta Perder
+pilas.colisiones.agregar(FlappyBird, Tubos2, Perder)
+#Colision del actor FlappyBird con Piso y de esta manera ejecuta Perder
+pilas.colisiones.agregar(FlappyBird, Pisos, Perder)
 
 pilas.ejecutar()
--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 import pilasengine
-pilas = pilasengine.iniciar()
+pilas = pilasengine.iniciar(ancho=288, alto=511)
 def iniciar_juego():
 	pilas.escenas.Juego()
 
@@ -11,7 +13,209 @@ def salir_del_juego():
 
 class Juego(pilasengine.escenas.Escena):
     def iniciar(self):
-        pilas.fondos.Tarde()   
+    	Fin_del_juego = False
+	#Añadir un marcador
+	puntos = pilas.actores.Puntaje(x=100, y=240, color=pilas.colores.naranja)
+	puntos.magnitud = 40
+	#Añadir el conmutador de sonido
+	Sonido = pilas.actores.Sonido
+	#Variables y constantes
+	Tubos1 = []
+	Tubos2 = []
+	Pisos = []
+	Puntos = []
+	#Define todas las cualidades del Fondo
+	class FondoConMovimiento(pilasengine.fondos.Fondo):
+
+	    def iniciar(self):
+	    	 # define que imagen se usara como fondo
+	        self.imagen = 'flappyfondo.png' 
+	        # velocidad a la que se movera el fondo
+	        self.velocidad = 1  
+
+	    def actualizar(self):
+	    	# define que se movera hacia la izquierda
+	        self.x -= self.velocidad  
+	        # Si el fondo llega al borde izquierdo de la pantalla entonces cambiar su posicion en X
+	        if self.x + self.ancho <= 0:
+	            self.x = self.ancho
+
+	#Define todas las cualidades de los Tubos (Colisionadores)
+	class Tubo(pilasengine.actores.Actor):
+
+	    def iniciar(self):
+	    	# Define la imagen que se usara como Tubo
+	        self.imagen = 'flappytubo.png'
+	        # Define velocidad con la que se movera el tubo  
+	        self.velocidad = 1  
+	        # Posicion en X en la que aparecera
+	        self.x = 288 
+
+	        # posicion en Y aleatoria entre 159 y 250
+	        self.y = self.pilas.azar(95, 350)
+	    def actualizar(self):
+	    	# mover tubos constantemente
+	        self.x -= self.velocidad  
+	        # Si el tubo llega al al borde izquierdo de la pantalla entonces lo eliminamos
+	        if self.x + self.ancho <= -144:
+	            self.eliminar()
+	    def crear_tubos():
+	    	#funcion que sera llamada como una tarea
+	        tubo1 = Tubo(pilas)
+	        #Define que el radio de colision sea rectangular
+	        colision_rec1 = pilas.fisica.Rectangulo(-1000,-1000,50,315, sensor=True, dinamica= False)
+	        tubo1.figura_de_colision = colision_rec1
+	        #Agrega los tubos al vector, para asi de este modo se puedan eliminar luego
+	        Tubos1.append(tubo1)
+	        #Tubo de abajo
+	        tubo2 = Tubo(pilas)
+	        #Define que el radio de colision sea rectangular
+	        colision_rec2 = pilas.fisica.Rectangulo(-1000,-1000,50,315, sensor=True, dinamica= False)
+	        tubo2.figura_de_colision = colision_rec2
+	        #Define que el tubo debe rotar para poder quedar con la forma deseada de tubo
+	        tubo2.rotacion = 180
+	        #Define la distancia 
+	        tubo2.y = tubo1.y - (tubo1.alto / 2) * 2 - 100
+	        #Agrega los tubos al vector, para asi de este modo se puedan eliminar luego
+	        Tubos2.append(tubo2)
+	        #Llamar a la funcion crear_tubos cada 2.5 segundos
+	    pilas.tareas.siempre(4, crear_tubos)
+	#Define todas las cualidades del Piso (Colisionadores)
+	class Piso(pilasengine.actores.Actor):
+	    def iniciar(self):
+	    	#Define la imagen que se usara como Piso
+	        self.imagen = 'flappypiso.png'
+	        #Define la velocidad con la que se movera el Piso
+	        self.velocidad = 1
+	        #Posicion X en la que aparecera
+	        self.x = 288
+	        #Posicion Y en la que aparecera
+	        self.y = -270
+	    def actualizar(self):
+	        # mover tubos constantemente
+	        self.x -= self.velocidad  
+	        # Si el Piso llega al al borde izquierdo de la pantalla entonces lo eliminamos
+	        if self.x + self.ancho <= -144:
+	            self.eliminar()
+	    def crear_piso():
+	    	#Funcion que será llamada como Tarea
+	        piso1 = Piso(pilas)
+	        #Define que la colision sea rectangular
+	        colision_rec9 = pilas.fisica.Rectangulo(-1000,-1000,185,100, sensor=True, dinamica= False)
+	        piso1.figura_de_colision = colision_rec9
+	        #Agrega el piso a un vector, para que de este modo se pueda eliminar luego
+	        Pisos.append(piso1)
+	        #Llamar a la funcion crear_piso cada 2.5 segundos
+	    pilas.tareas.siempre(3, crear_piso)  
+
+	class ganar_puntos(pilasengine.actores.Actor):
+	    def iniciar(self):
+	        # Define la imagen que se usara como Actor para generar puntos
+	        self.imagen = 'invisible.png'
+	        # Define velocidad con la que se movera el tubo  
+	        self.velocidad = 1  
+	        # Posicion en X en la que aparecera
+	        self.x = 288 
+	        # posicion en Y aleatoria entre -160 y -30
+	        self.y = self.pilas.azar(-160, -30)
+	    def actualizar(self):
+	        # mover puntos constantemente
+	        self.x -= self.velocidad  
+	        # Si el tubo llega al al borde izquierdo de la pantalla entonces lo eliminamos
+	        if self.x + self.ancho <= -144:
+	            self.eliminar()
+	def crear_ganar_puntos():
+	    gana = ganar_puntos(pilas)
+	    #Define que la colision sea rectangular
+	    colision_rec7 = pilas.fisica.Rectangulo(-1000,-1000,50,95, sensor=True, dinamica= False)
+	    gana.figura_de_colision = colision_rec7
+	    #Agrega crear_ganar_puntos a un vector para ser eliminados luego
+	    Puntos.append(gana)
+	    #Aumenta el contador en 1
+	    puntos.aumentar(1)
+	    #Llama a la funcion crear_ganar_piso en 2.5 segundos 
+	pilas.tareas.siempre(4, crear_ganar_puntos)
+
+
+	# Crear fondo 1 con posicion en X de 288 (no visible)
+	fondo55 = FondoConMovimiento(pilas)
+	fondo55.x = 288
+	# Crear fondo 2
+	fondo22 = FondoConMovimiento(pilas)
+	#Define los controles que usara el juego
+	teclas = {
+	            pilas.simbolos.w: 'arriba',
+	            pilas.simbolos.s: 'abajo',
+	        }
+	mi_control = pilas.control.Control(teclas)
+	#Define al personaje principal FlappyBird
+	class FlappyConControles(pilasengine.actores.Actor):
+		#Clase que define todos los aspectos del Actor
+	    def iniciar(self):
+	    	#Define la imagen que se utilizara para el actor"
+	        self.imagen = pilas.imagenes.cargar_grilla("flappybird.png", 3)
+	        #Define la escala de tamaño del actor
+	        self.escala = 1.2
+	        #Define la posicion X en la cual se posicionara el actor
+	        self.x = -80
+	        #Define la posicion Y en la cual se posicionara el actor
+	        self.y = -200
+	        
+	    def actualizar(self):
+	    	#Define cuanto avanzara en 0
+	        self.x += 0
+	        #Define cuanto avanzara el actor
+	        self.imagen.avanzar()
+	        #Define los controles
+	        #Define que pasara cuando se toque la letra "S"
+	        if mi_control.abajo:
+	        	#Define cuanto se movera en el eje Y, mediante la interpolacion
+				self.v=self.y-20
+				#Define cuanto se movera el personaje gracias a esta interpolacion
+				pilas.utils.interpolar(FlappyBird,'y',self.v,0.1)
+			#Define que pasara cuando se toque la letra "W"
+	        if mi_control.arriba:
+	        	#Define cuanto se movera en el eje Y, mediante la interpolacion
+				self.z=self.y+90
+				#Define cuanto se movera el personaje gracias a esta interpolacion
+				pilas.utils.interpolar(FlappyBird,'y',self.z,0.4)
+				#Define que sonido se escuchara cuando se toque la tecla "W"
+				self.sonido_saltar = self.pilas.sonidos.cargar("audio/saltar.wav")
+				#Le indica que se comiense a reproducir el sonido del salto
+				self.sonido_saltar.reproducir()
+			
+	#Funcion utilizada para cuando el usuario Pierde
+	def Perder(FlappyBird, tubos):	
+	    global Fin_del_juego
+	    FlappyBird.y = 0
+	    tubos.eliminar()
+	    #Emerger mensaje cuando el FlappyBird colisiona con un tubo
+	    texto1=pilas.actores.Texto("GAME OVER")
+	    texto1.y= 100
+	    #Informarle al usuario sobre su puntuacion obtenida
+	    texto2=pilas.actores.Texto("Perdiste conseguiste %d puntos" % (puntos.obtener()))
+	    texto2.y= -100
+	    #Define que el juego a finalizado.
+	    Fin_del_juego=True
+
+	    if Fin_del_juego:
+	        return False
+	    else:
+	        return True
+	#Vincula a FlappyConControles como un Actor
+	pilas.actores.vincular(FlappyConControles)
+	#Vincula a Tubo como un Actor
+	pilas.actores.vincular(Tubo)
+	#Vincula a Piso como un Actor
+	pilas.actores.vincular(Piso)
+	#Define como se llamara FlappyConControles
+	FlappyBird = FlappyConControles(pilas)
+	#Colision del actor FlappyBird con los Tubos1 y de esta manera ejecuta Perder
+	pilas.colisiones.agregar(FlappyBird, Tubos1, Perder)
+	#Colision del actor FlappyBird con los Tubos2 y de esta manera ejecuta Perder
+	pilas.colisiones.agregar(FlappyBird, Tubos2, Perder)
+	#Colision del actor FlappyBird con Piso y de esta manera ejecuta Perder
+	pilas.colisiones.agregar(FlappyBird, Pisos, Perder)   
 
 class Instrucciones(pilasengine.escenas.Escena):
     def iniciar(self):       
